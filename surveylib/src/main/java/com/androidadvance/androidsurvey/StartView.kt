423ca2cde04b98a34b02e2f0d694128e29aa346a
Jose Mari Lumanlan <64821085+josemarilumanlan@users.noreply.github.com>
2021-05-28 00:06:43 +0800

    Soooo Many

--- surveylib/src/main/java/com/androidadvance/androidsurvey/StartView.kt
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/StartView.kt
@@ -11,22 +11,18 @@ import com.androidadvance.androidsurvey.models.SurveyProperties
 
 class StartView(val surveyView: SurveyView, val surveyProperties: SurveyProperties) : FrameLayout(surveyView.context) {
 
-//    override fun onAttachedToWindow() {
-//        super.onAttachedToWindow()
-//        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//        val root  = inflater.inflate(R.layout.fragment_start, null)
-//
-//        val textView_start = root.findViewById(R.id.textView_start) as TextView
-//        val button_continue = root.findViewById(R.id.button_continue) as Button
-//
-//        textView_start.text = Html.fromHtml(surveyProperties.introMessage)
-//
-//        button_continue.setOnClickListener{ surveyView.go_to_next() }
-//        this.addView(root)
-//    }
-
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val root  = inflater.inflate(R.layout.fragment_start, null)
+        val textView_start = root.findViewById(R.id.textView_start) as TextView
+        val button_continue = root.findViewById(R.id.button_continue) as Button
 
+        textView_start.text = Html.fromHtml(surveyProperties.introMessage)
+
+        button_continue.setOnClickListener{ surveyView.go_to_next() }
+        this.addView(root)
     }
+
+
 }
--- surveylib/src/main/java/com/androidadvance/androidsurvey/SurveyView.kt
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/SurveyView.kt
@@ -1,24 +1,125 @@
 package com.androidadvance.androidsurvey
 
 import android.content.Context
-import android.widget.Button
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.viewpager.widget.ViewPager
+import com.androidadvance.androidsurvey.adapters.MainPagerAdapter
+import com.androidadvance.androidsurvey.models.SurveyPojo
+import com.androidadvance.androidsurvey.views.*
+import com.google.gson.Gson
 
-class SurveyView constructor(context: Context) : FrameLayout(context) {
 
+class SurveyView constructor(context: Context, bundle: Bundle) : FrameLayout(context) {
 
-    lateinit var button: Button
+    var mSurveyPojo: SurveyPojo
+    var mPager: ViewPager
+    var pagerAdapter: MainPagerAdapter
+    var style_string: String? = null
 
-//    <com.androidadvance.androidsurvey.widgets.NoSwipeViewPager
-//    xmlns:android="http://schemas.android.com/apk/res/android"
-//    android:id="@+id/pager"
-//    android:layout_width="match_parent"
-//    android:layout_height="match_parent" />
 
     init {
-        val frameLayoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        button = Button(context.applicationContext).apply { this.layoutParams = frameLayoutParams }
-        this.addView(button, frameLayoutParams)
+        val frameLayoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
+        mSurveyPojo = Gson().fromJson(bundle.getString("json_survey"), SurveyPojo::class.java)
+        if (bundle.containsKey("style")) {
+            style_string = bundle.getString("style")
+        }
+
+        Log.i("json Object = ", mSurveyPojo.questions.toString())
+
+        mPager = ViewPager(context)
+        pagerAdapter = MainPagerAdapter()
+        mPager.adapter = pagerAdapter
+
+        this.addView(mPager, frameLayoutParams)
+
+
+        //- START -
+        if (!mSurveyPojo.surveyProperties.skipIntro)
+            addToPager(StartView(this, mSurveyPojo.surveyProperties))
+
+        for (mQuestion in mSurveyPojo.questions) {
+
+            when (mQuestion.questionType) {
+
+                "String" -> addToPager(TextSimpleView(this, mQuestion))
+
+                "Checkboxes" -> addToPager(CheckboxesView(this, mQuestion))
+
+                "Radioboxes" -> addToPager(RadioBoxesView(this, mQuestion))
+
+                "Number" -> addToPager(NumberView(this, mQuestion))
+
+                "StringMultiline" -> addToPager(MultilineView(this, mQuestion))
+            }
+
+        }
+        //- END -
+        addToPager(EndView(this, mSurveyPojo.surveyProperties))
+
+
+    }
+
+    private fun addToPager(view: View) {
+        pagerAdapter.addView(view)
+        pagerAdapter.notifyDataSetChanged()
+    }
+
+    fun go_to_next() {
+        mPager.currentItem = mPager.currentItem + 1
+    }
+
+    fun event_survey_completed(instance: Answers) {
+        val returnIntent = Intent()
+        returnIntent.putExtra("answers", instance._json_object)
+        Log.d(LOG_TAG, "event_survey_completed: ${instance._json_object}")
+        Toast.makeText(context, "${instance._json_object}", Toast.LENGTH_SHORT).show()
+        startActivity(context, Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME),null)
+    }
+
+
+    //USAGE COLLECTION
+
+    //-----------------------------------------------------------------------------
+    // Here's what the app should do to add a view to the ViewPager.
+    override fun addView(newPage: View?) {
+        val pageIndex: Int = pagerAdapter.addView(newPage)
+        // You might want to make "newPage" the currently displayed page:
+        mPager.setCurrentItem(pageIndex, true)
+    }
+
+    //-----------------------------------------------------------------------------
+    // Here's what the app should do to remove a view from the ViewPager.
+    override fun removeView(defunctPage: View?) {
+        var pageIndex: Int = pagerAdapter.removeView(mPager, defunctPage)
+        // You might want to choose what page to display, if the current page was "defunctPage".
+        if (pageIndex == pagerAdapter.getCount()) pageIndex--
+        mPager.setCurrentItem(pageIndex)
+    }
+
+    //-----------------------------------------------------------------------------
+    // Here's what the app should do to get the currently displayed page.
+    fun getCurrentPage(): View? {
+        return pagerAdapter.getView(mPager.getCurrentItem())
+    }
+
+    //-----------------------------------------------------------------------------
+    // Here's what the app should do to set the currently displayed page.  "pageToShow" must
+    // currently be in the adapter, or this will crash.
+    fun setCurrentPage(pageToShow: View) {
+        mPager.setCurrentItem(pagerAdapter.getItemPosition(pageToShow), true)
+    }
+
+    companion object {
+        const val LOG_TAG = "SurveyView"
+
     }
 
 }
